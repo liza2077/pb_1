ff7a0bc27d42ae64d4fe11bd648c9bad1be69cde
Сергеев Дмитрий <dmitrijsergeev42@gmail.com>
2024-12-02 10:17:31 +0300

    поле 1

--- src/main.cpp
+++ src/main.cpp
@@ -65,7 +65,7 @@ while(BeepersPresent()==true) {
 /// Стартовая функция
 ///
 int main() {
-    LoadWorld("worlds/CB_02.w");
+    LoadWorld("worlds/CB_01.w");
     KarelProgram();
     Finish();
     return 0;
